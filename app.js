0cd434e6bf3944c86cf058099c1f2316ec93f8fa
Giuseppe Crinò <giuscri@gmail.com>
2015-04-13 21:44:45 +0200

    Flush the buffer.

---
+++ app.js
@@ -0,0 +1,12 @@
+var hackernews = angular.module("hackernews", ["ngRoute", "hackernewsControllers"]);
+hackernews.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/main.html",
+            controller: "MainCtrl"
+        })
+        .when("/:id/comments", {
+            templateUrl: "templates/comments.html",
+            controller: "DetailCtrl"
+        });
+}]);
