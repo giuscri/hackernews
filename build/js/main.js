bbfe20e596b03e7ba82687a60f8ee8f3e58f38e9
Giuseppe Crinò <giuscri@gmail.com>
2015-04-14 14:50:06 +0200

    Added build directory .......

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+var hackernews=angular.module("hackernews",["ngRoute","hackernewsControllers"]);hackernews.config(["$routeProvider",function(a){a.when("/",{templateUrl:"templates/main.html",controller:"MainCtrl"}).when("/:id/comments",{templateUrl:"templates/comments.html",controller:"DetailCtrl"})}]);
